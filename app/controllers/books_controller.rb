783b35da4ccc96674208ab929653d0cfbb524f3b
sdmccloskey <sdmccloskey@gmail.com>
2018-01-20 11:36:02 -0500

    index working

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,39 @@
 class BooksController < ApplicationController
-  def index
-  end
+  skip_before_action :verify_authenticity_token
 
-  def show
-  end
+    def index
+      response = RestClient.get('https://skookum-test-api.herokuapp.com/api/v1/books')
+      @books = JSON.parse(response.body)
+    end
 
-  def new
-  end
+    def show
+      books_response = RestClient.get("https://skookum-test-api.herokuapp.com/api/v1/books")
+      comment_response = RestClient.get("https://skookum-test-api.herokuapp.com/api/v1/books")
 
-  def edit
-  end
+      @books = JSON.parse(books_response.body)
+      @comments = JSON.parse(comment_response.body)
+    end
 
-  def destroy
+    def create
+      books = params['books']
+
+      response = RestClient.books("https://jsonplaceholder.typicode.com/posts", books)
+
+      books_id = JSON.parse(response)["id"]
+
+      redirect_to books_path(books_id)
+    end
+
+    def edit
+      # get the value of item by making a call to the API and storing in a variable
+      # ie: @post = RestClient.get('endpoint/:id')
+    end
+
+    def update
+      # @post is already available to you
+
+    end
+
+    def destroy
+    end
   end
-end
