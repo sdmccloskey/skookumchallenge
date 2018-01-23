f7cbe26a1017be304f0c28d54d8afe41a7b7ea56
sdmccloskey <sdmccloskey@gmail.com>
2018-01-23 17:55:13 -0500

    cleaned up books controller, no end points in api, adjusted json parse

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,38 +2,27 @@ class BooksController < ApplicationController
   skip_before_action :verify_authenticity_token
 
     def index
-      response = RestClient.get('https://skookum-test-api.herokuapp.com/api/v1/books')
-      @books = JSON.parse(response.body)
+      fetch_books
     end
 
     def show
-      books_response = RestClient.get("https://skookum-test-api.herokuapp.com/api/v1/books")
-      comment_response = RestClient.get("https://skookum-test-api.herokuapp.com/api/v1/books")
-
-      @books = JSON.parse(books_response.body)
-      @comments = JSON.parse(comment_response.body)
+      set_book
     end
 
-    def create
-      books = params['books']
+  private
 
-      response = RestClient.books("https://jsonplaceholder.typicode.com/posts", books)
+  def parse_books(books)
+    JSON.parse(books.body)
+  end
 
-      books_id = JSON.parse(response)["id"]
+  def fetch_books
+    @books ||= parse_books(RestClient.get("https://skookum-test-api.herokuapp.com/api/v1/books"))
+  end
 
-      redirect_to books_path(books_id)
-    end
-
-    def edit
-      # get the value of item by making a call to the API and storing in a variable
-      # ie: @post = RestClient.get('endpoint/:id')
-    end
-
-    def update
-      # @post is already available to you
-
-    end
-
-    def destroy
+  def set_book
+    @book = fetch_books.detect do |book|
+      book['title'].include?(params['title'].gsub('-',' '))
     end
   end
+
+end
